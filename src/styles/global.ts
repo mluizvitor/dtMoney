bfbd1c250b7a8a367b2c840a7874fc5ff0de3e2e
Vitor Monteiro <mluizvitor@gmail.com>
2022-02-23 21:23:22 -0300

    Correção nas variáveis de cor

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,7 +6,7 @@ export const GlobalStyle = createGlobalStyle`
     --shape: #FFFFFF;
     
     --blue: #5429CC;
-    --blue-light: #6933FF
+    --blue-light: #6933FF;
     --green: #33CC95;
     --red: #E62E4D;
     
